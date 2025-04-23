596e14f39ae3014aa805bc8391b7175a93eb2ab1
Ignacia22 <mfernandezpolanco@gmail.com>
2025-04-23 18:00:33 -0400

    Fix DB_ENTITIES path for both dev and production environments

--- back/src/config/envs.ts
+++ back/src/config/envs.ts
@@ -11,5 +11,8 @@ export const DB_PASSWORD: string | undefined = process.env.DB_PASSWORD;
 export const DB_DATABASE: string | undefined = process.env.DB_DATABASE;
 export const DB_SYNC: boolean = process.env.DB_SYNC ? process.env.DB_SYNC === "true": true;
 export const DB_LOGGING: boolean = process.env.DB_LOGGING ? process.env.DB_LOGGING === "true": true;
-export const DB_ENTITIES: string[] = process.env.DB_ENTITIES ? process.env.DB_ENTITIES.split(","): ["src/entities/**/*.ts"];
+export const DB_ENTITIES: string[] = process.env.DB_ENTITIES
+  ? process.env.DB_ENTITIES.split(",")
+  : [__dirname + "/../entities/**/*.{ts,js}"];
+
 export const DB_DROP: boolean = process.env.DB_DROP ? process.env.DB_DROP === "true": false;
