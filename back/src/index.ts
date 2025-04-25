5f69163173f27343d0113572e9235068c58fc2b2
Ignacia22 <mfernandezpolanco@gmail.com>
2025-04-24 20:15:56 -0400

    I don't know

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,22 +1,15 @@
-import {PORT} from "./config/envs";
-import server from "./server";
-
+import { PORT } from "./config/envs.js"; // Añade .js
+import server from "./server.js"; // Añade .js
 import "reflect-metadata";
-import { AppDataSource } from "./config/data.source";
+import { AppDataSource } from "./config/data.source.js"; // Añade .js
 
 AppDataSource.initialize()
-   .then(() => {
-
+  .then(() => {
     console.log("Database connection successful");
-    
-
-
     server.listen(PORT, () => {
-        console.log(`Servidor corriendo en el puerto ${PORT}`);
+      console.log(`Servidor corriendo en el puerto ${PORT}`);
     });
-   })
-
-   .catch((err) => {
+  })
+  .catch((err) => {
     console.log(err);
-   })
-
+  });
