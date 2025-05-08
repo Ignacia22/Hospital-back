01ace6a6af53209eca33b55fee7e68d992264b37
Ignacia22 <mfernandezpolanco@gmail.com>
2025-05-08 18:29:17 -0400

    prueba-2

--- back/src/config/data.source.ts
+++ back/src/config/data.source.ts
@@ -4,7 +4,7 @@ import { Credential } from "../entities/Credential.entity";
 import { DB_ENTITIES, DB_SYNC, DB_LOGGING, DB_DROP } from "./envs";
 
 // Usar la URL de conexión directamente
-const DATABASE_URL = process.env.DATABASE_URL || 'postgresql://neondb_owner:npg_DG6KECamc3gf@ep-winter-waterfall-a42s4j4m-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require';
+const DATABASE_URL = process.env.DATABASE_URL || 'https://hospital-back2.onrender.com';
 
 export const AppDataSource = new DataSource({
     type: "postgres",
